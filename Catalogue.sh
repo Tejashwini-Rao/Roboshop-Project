9622ebbbe3c3897040b5f5b66ae040b1e136540d
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-05 22:52:04 +0530

    All Components scripts

--- Catalogue.sh
+++ Catalogue.sh
@@ -5,7 +5,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
  curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
  cd /home/roboshop
- unzip /tmp/catalogue.zip
+ unzip -o /tmp/catalogue.zip
  mv catalogue-main catalogue
  cd /home/roboshop/catalogue
  npm install
--- User.sh
+++ User.sh
@@ -5,7 +5,7 @@
 
  curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
  cd /home/roboshop
- unzip /tmp/user.zip
+ unzip -o /tmp/user.zip
  mv user-main user
  cd /home/roboshop/user
  npm install
