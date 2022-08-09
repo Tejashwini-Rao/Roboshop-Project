0f6379a0693959fe5d84903848db78b5a907629d
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-09 21:47:16 +0530

    updating ./Roboshop-project/Roboshop-Project

--- Catalogue.sh
+++ Catalogue.sh
@@ -5,6 +5,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
  curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
  cd /home/roboshop
+ rm -rf catalogue
  unzip -o /tmp/catalogue.zip
  mv catalogue-main catalogue
  cd /home/roboshop/catalogue
