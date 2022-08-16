360b1d87cb0e097d8e5ad6ffd1febdf7e67eca0e
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-16 22:04:01 +0530

    updating ./Roboshop-project/Roboshop-Project

--- Cart.sh
+++ Cart.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
  set -e
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/Cart.log
- yum install nodejs -y>/temp/Cart.log
+ yum install nodejs -y>/tmp/Cart.log
 
- useradd roboshop>/temp/Cart.log
+ useradd roboshop>/tmp/Cart.log
 
- curl -s -L -o /tmp/Cart.zip "https://github.com/roboshop-devops-project/Cart/archive/main.zip">/temp/Cart.log
- cd /home/roboshop>/temp/Cart.log
- rm -rf Cart>/temp/Cart.log
- unzip -o /tmp/Cart.zip>/temp/Cart.log
- mv Cart-main Cart>/temp/Cart.log
- cd Cart>/temp/Cart.log
- npm install>/temp/Cart.log
+ curl -s -L -o /tmp/Cart.zip "https://github.com/roboshop-devops-project/Cart/archive/main.zip">/tmp/Cart.log
+ cd /home/roboshop>/tmp/Cart.log
+ rm -rf Cart>/tmp/Cart.log
+ unzip -o /tmp/Cart.zip>/tmp/Cart.log
+ mv Cart-main Cart>/tmp/Cart.log
+ cd Cart>/tmp/Cart.log
+ npm install>/tmp/Cart.log
 
- mv /home/roboshop/Cart/systemd.service /etc/systemd/system/Cart.service>/temp/Cart.log
- systemctl daemon-reload>/temp/Cart.log
- systemctl start Cart>/temp/Cart.log
- systemctl enable Cart>/temp/Cart.log
+ mv /home/roboshop/Cart/systemd.service /etc/systemd/system/Cart.service>/tmp/Cart.log
+ systemctl daemon-reload>/tmp/Cart.log
+ systemctl start Cart>/tmp/Cart.log
+ systemctl enable Cart>/tmp/Cart.log
