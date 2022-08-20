a611119c43355e12dc391d73c3f6454a317a17ab
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-20 23:50:25 +0530

    Changes

--- MySql.sh
+++ MySql.sh
@@ -9,7 +9,7 @@
  # shellcheck disable=SC2034
  DEFAULT_PASSWORD=$(grep ' A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
- echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '-pRoboShop@1'; "| mysql -uroot -p${DEFAULT_PASSWORD}
+ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '-pRoboShop@1'; "| mysql  --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
 
  mysql -uroot -pRoboShop@1
 #> uninstall plugin validate_password;
