1233aa0dfddd4e8a23326d5fb5c252c6c62f1937
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-21 21:06:33 +0530

    Changes

--- Cart.sh
+++ Cart.sh
@@ -30,6 +30,13 @@
  npm install>/tmp/Cart.log
  echo $?
 
+  echo configuring cart services
+  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+  systemctl daemon-reload
+  systemctl start cart
+  systemctl enable cart
+  echo $?
+
 echo configuring cart services
  mv /home/roboshop/Cart/systemd.service /etc/systemd/system/Cart.service>/tmp/Cart.log
  systemctl daemon-reload>/tmp/Cart.log
