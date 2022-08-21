cc244ffe28fba689a3ec103b0ac3f0ecde82415c
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-21 20:42:24 +0530

    Changes

--- Cart.sh
+++ Cart.sh
@@ -3,26 +3,32 @@
 
  echo setting nodeJS repos
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/Cart.log
+ echo $?
 
  echo installing NodeJS
  yum install nodejs -y>/tmp/Cart.log
+ echo $?
 
-echo adding user
+ echo adding user
  useradd roboshop>/tmp/Cart.log
+ echo $?
 
-echo downloding application content
+ echo downloding application content
  curl -s -L -o /tmp/Cart.zip "https://github.com/roboshop-devops-project/Cart/archive/main.zip">/tmp/Cart.log
  cd /home/roboshop>/tmp/Cart.log
+ echo $?
 
-echo extracting application archives
+ echo extracting application archives
  recho removing files
  rm -rf Cart>/tmp/Cart.log
  unzip -o /tmp/Cart.zip>/tmp/Cart.log
  mv Cart-main Cart>/tmp/Cart.log
  cd Cart>/tmp/Cart.log
+ echo $?
 
  echo installing NodeJS dependencies
  npm install>/tmp/Cart.log
+ echo $?
 
 echo configuring cart services
  mv /home/roboshop/Cart/systemd.service /etc/systemd/system/Cart.service>/tmp/Cart.log
