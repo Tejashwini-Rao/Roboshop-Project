5fa5baac3ab43abcc6222befaade1401af756e15
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-21 23:39:59 +0530

    Changes

--- Cart.sh
+++ Cart.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-StatusCheck(){
-  if [ $? -eq 0 ]
-    then
-      echo -e "\e[32mSUCCESS\e[0m"
-    else
-      echo -e "\e[31mFAILURE\e[0m"
-      exit 1
-    fi
-}
+source Common.sh
 
  echo setting nodeJS repos
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/cart.log
