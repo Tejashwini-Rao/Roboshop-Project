0e2c0d4ac973f4e33c36892274bafb15cdd6389a
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-21 22:39:00 +0530

    Changes

--- Cart.sh
+++ Cart.sh
@@ -6,40 +6,78 @@
  if [ $? -eq 0 ]
  then
    echo -e "\e[32mSUCCESS\e[0m"
+ else
+   -e "\e[31mFAILURE\e[0m"
  fi
 
  echo installing NodeJS
  yum install nodejs -y&>>/tmp/cart.log
- echo $?
+
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    -e "\e[31mFAILURE\e[0m"
+  fi
 
  echo adding user
  useradd roboshop&>>/tmp/cart.log
- echo $?
+
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    -e "\e[31mFAILURE\e[0m"
+  fi
 
  echo downloding application content
  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"&>>/tmp/cart.log
  cd /home/roboshop&>>/tmp/cart.log
- echo $?
 
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    -e "\e[31mFAILURE\e[0m"
+  fi
  echo cleaning old application
  rm -rf cart&>>/tmp/cart.log
- echo $?
 
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    -e "\e[31mFAILURE\e[0m"
+  fi
 echo extracting application archives
  unzip -o /tmp/cart.zip&>>/tmp/cart.log
  mv cart-main cart&>>/tmp/cart.log
  cd cart&>>/tmp/cart.log
- echo $?
 
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    -e "\e[31mFAILURE\e[0m"
+  fi
  echo installing NodeJS dependencies
  npm install&>>/tmp/cart.log
- echo $?
-
+ if [ $? -eq 0 ]
+  then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    -e "\e[31mFAILURE\e[0m"
+  fi
   echo configuring cart services
   mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
   systemctl daemon-reload
   systemctl start cart
   systemctl enable cart
-  echo $?
 
+  if [ $? -eq 0 ]
+   then
+     echo -e "\e[32mSUCCESS\e[0m"
+   else
+     -e "\e[31mFAILURE\e[0m"
+   fi
 
