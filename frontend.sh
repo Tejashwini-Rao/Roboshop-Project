7505548b4a049f968f9566fd34fb935da84ed442
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-22 21:16:58 +0530

    Changes

--- frontend.sh
+++ frontend.sh
@@ -13,7 +13,8 @@
 
    echo Extract Downloaded content
    unzip /tmp/frontend.zip&>>${LOG} && mv frontend-main/static/* . && mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+   Stating
 
    echo Stating Nginx
    systemctl restart nginx &>>${LOG} && systemctl enable nginx &>>{LOG}
-
+   Stating
