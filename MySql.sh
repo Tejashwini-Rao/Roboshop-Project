6babbbee8fc143a148492e15249970a8b52da2bf
Tejashwini Rao <polepalle.07@gmail.com>
2022-08-22 22:19:27 +0530

    Changes

--- MySql.sh
+++ MySql.sh
@@ -1,22 +1,40 @@
- set -e
- curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+ COMPONENT=mysql
+ source common.sh
 
- yum install mysql-community-server -y
+ if [ -z "$MYSQL_PASSWORD" ]; then
+   echo -e "\e[33m env variable MYSQL_PASSWORD is missing \e[0m"
+   exit 1
+ fi
 
- systemctl enable mysqld
- systemctl start mysqld
+ echo Setup YUM Repo
+ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+ StatusCheck
 
- # shellcheck disable=SC2034
- DEFAULT_PASSWORD=$(grep ' A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+ echo Install MySQL
+ yum install mysql-community-server -y &>>${LOG}
+ StatusCheck
 
-echo MYSQL_PASSWORD= $MYSQL_PASSWORD
- echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_PASSWORD'; "| mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+ echo Start MySQL Service
+ systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+ StatusCheck
 
- echo "uninstall plugin validate_password"|mysql -uroot -p$MYSQL_PASSWORD
-#> uninstall plugin validate_password;
- curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+ echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
+ if [ $? -ne 0 ]; then
+   echo Changing Default Password
+   DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+   echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>${LOG}
+   StatusCheck
+ fi
 
- cd /tmp
- unzip -o  mysql.zip
- cd mysql-main
- mysql -u root -pRoboShop@1 <shipping.sql
+ echo "show plugins;" | mysql -uroot -p$MYSQL_PASSWORD 2>&1 | grep validate_password &>>${LOG}
+ if [ $? -eq 0 ]; then
+   echo Remove Password Validate Plugin
+   echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
+   StatusCheck
+ fi
+
+ DOWNLOAD
+
+ echo "Extract & Load Schema"
+ cd /tmp &>>${LOG} && unzip -o mysql.zip &>>${LOG} &&  cd mysql-main &>>${LOG} && mysql -u root -p$MYSQL_PASSWORD <shipping.sql &>>${LOG}
+ StatusCheck
--- Redis.sh
+++ Redis.sh
@@ -1,7 +1,21 @@
 set -e
- curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
- yum install redis-6.2.7 -y
 
- systemctl enable redis
- systemctl start redis
 
+COMPONENT=redis
+source common.sh
+
+echo Setup YUM Repo
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+StatusCheck
+
+echo Install Redis
+yum install redis-6.2.7 -y &>>${LOG}
+StatusCheck
+
+echo Update Redis Listen Address
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+StatusCheck
+
+echo Start Redis Service
+systemctl enable redis &>>${LOG} && systemctl restart redis &>>${LOG}
+StatusCheck
